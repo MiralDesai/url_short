bab141261d6eee7d60d538ab344816d6fbefc259
Miral Desai <miral.desai4@gmail.com>
2018-08-18 11:04:33 +0100

    Added a bit more formatting to the original path

--- app/models/url.rb
+++ app/models/url.rb
@@ -4,8 +4,21 @@ class Url < ApplicationRecord
 
   before_save :format_path
 
-  # Force https because I feel it's expected these days
+  def shorten(url:)
+    # TODO: Validate url or raise error
+    url = UrlShortener.new(url: url).shorten
+  end
+
+  private
+
   def format_path
+    path.strip! # Remove spaces
+    path.chomp!('/') # Remove trailing slash
+    self.path = httpsify_path(path)
+  end
+
+  # Force https because I feel it's expected these days
+  def httpsify_path(path)
     uri = URI.parse(path)
     url.gsub!('http', 'https') if %w[http].include?(uri.scheme)
     url.prepend('https://') unless uri.scheme
