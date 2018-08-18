88f16298055e9e1736a9eb9689a6e723e04bff88
Miral Desai <miral.desai4@gmail.com>
2018-08-18 10:14:35 +0100

    short_path uniqueness can be case sensitive, allows for more options, also added basic validation for the url, adding https to the front of all urls

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,13 @@
 class Url < ApplicationRecord
   validates :path, uniqueness: { case_sensitive: false }
-  validates :short_path, uniqueness: { case_sensitive: false }
+  validates :short_path, uniqueness: { case_sensitive: true }
+
+  before_save :format_path
+
+  # Force https because I feel it's expected these days
+  def format_path
+    uri = URI.parse(path)
+    url.gsub!('http', 'https') if %w[http].include?(uri.scheme)
+    url.prepend('https://') unless uri.scheme
+  end
 end
