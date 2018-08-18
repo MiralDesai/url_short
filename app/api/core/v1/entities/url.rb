b99d528df8a04644eb6a503eb1a176595bfab519
Miral Desai <miral.desai4@gmail.com>
2018-08-18 11:03:31 +0100

    Add post endpoint with basic idea of the flow

--- app/api/core/v1/entities/url.rb
+++ app/api/core/v1/entities/url.rb
@@ -4,6 +4,11 @@ module Core
       class Url < Grape::Entity
         expose :path
         expose :short_path
+        # expose :full_new_url
+
+        def full_new_url
+          "http://localhost:3000#{object.short_path}"
+        end
       end
     end
   end
--- app/api/core/v1/urls.rb
+++ app/api/core/v1/urls.rb
@@ -7,6 +7,18 @@ module Core
       get '/' do
         present :urls, Url.all, with: Entities::Url
       end
+
+      desc 'Create a shortened URL' do
+        entity Core::V1::Entities::Url
+      end
+      parmas do
+        requires :url, type: String, desc: 'The url you want shortened'
+      end
+      post '/' do
+        # # TODO: Does shortener already exist? Check here and automatically return
+        url = Url.shorten(url: params[:url])
+        present url, with: Entities::Url
+      end
     end
   end
 end
