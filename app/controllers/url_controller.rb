87ddf7b26371f77dd7e6bb28521783bcd43ec064
Miral Desai <miral.desai4@gmail.com>
2018-08-19 14:15:23 +0100

    Got bored and decided to create an index page and new/create form, no use case for edit

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -4,4 +4,33 @@ class UrlController < ApplicationController
     Url.increment_counter(:clicks, @url.id)
     redirect_to @url.path
   end
+
+  def index
+    @urls = Url.all
+  end
+
+  def new
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.shorten(url: url_params[:path])
+    if @url.save
+      redirect_to url_index_path, notice: 'Url created'
+    else
+      render new_url_path, alert: 'Error creating url'
+    end
+  end
+
+  def destroy
+    Url.find(params[:id]).destroy
+    flash[:success] = 'Url deleted'
+    redirect_to url_index_path
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:path)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/:short_url', to: 'url#show'
 
+  resources :url
+  get '/:short_url', to: 'url#show'
   mount Core::ApiV1 => '/api'
   mount GrapeSwaggerRails::Engine => '/api/docs'
 end
